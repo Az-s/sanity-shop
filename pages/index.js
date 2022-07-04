d1f82f8db49cb91700b43900c8739a63a1ad1c1a
Az-s <azim.sa@yahoo.com>
2022-07-04 19:04:45 +0600

    created component layout

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,9 @@
-import { Typography } from '@mui/material'
-import Head from 'next/head'
+import Layout from '../components/Layout'
 
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>Sanity shop</title>
-        <meta name="description" content="The ecommerce website by next and sanity" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-        <Typography component='h1' variant='h1'>Sanity shop</Typography>
-      
-    </div>
+  <Layout>
+    List Products
+  </Layout>
   )
 }
---
+++ utils/classes.jsx
@@ -0,0 +1,26 @@
+const classes = {
+    main: {
+        marginTop: '2rem',
+        minHeight: '80vh',
+    },
+    footer: {
+        marginTop: '1rem',
+        textAlign: 'center'
+    },
+    appbar: {
+        backgroundColor: '#203040',
+        '& a': {
+                color: '#ffffff',
+                marginLeft: '1'
+        }
+    },
+    toolbar: {
+        justifyContent: 'space-between',
+    },
+    brand: {
+        fontWeight: 'bold',
+        fontSize: '1.5rem',
+    }
+};
+
+export default classes;
